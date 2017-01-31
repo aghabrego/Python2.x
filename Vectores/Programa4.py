ea24d9dec018e3d444264ae41adcc9d09aafeb9c
Angel Hidalgo <ah.criptoy@gmail.com>
2017-01-31 18:14:11 -0500

    Agregando programa que determina el mayor elemento de un vector.

---
+++ Vectores/Programa4.py
@@ -0,0 +1,16 @@
+# Programa que determina el mayor elemento de un vector.
+n = input("Cantidad de Veces ")
+i = 0
+vector = []
+mayor = 0
+
+for i in range(n):
+    elemento = input("Indique un numero ")
+    vector.append(elemento)
+    i = i + 1
+    
+for i in range(n):
+    if vector[i] > mayor:
+        mayor = vector[i]
+
+print "El valor mayor es ",mayor
