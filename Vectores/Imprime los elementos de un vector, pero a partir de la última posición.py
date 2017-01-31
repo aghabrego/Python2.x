5aa5c6f96ab3c4f17c07d14d980050075dd93c05
Angel Hidalgo <ah.criptoy@gmail.com>
2017-01-31 09:34:38 -0500

    Programa que imprime los elementos de un vector, pero a partir de la última posición.

---
+++ Vectores/Imprime los elementos de un vector, pero a partir de la última posición.py
@@ -0,0 +1,9 @@
+# -*- coding: cp1252 -*-
+# Programa que imprime los elementos de un vector, pero a partir de la última posición.
+vec = [5,4,6,8,9,7,10]
+i = 0
+
+while (i < 8):
+    i = i + 1
+
+print vec[::-1]
