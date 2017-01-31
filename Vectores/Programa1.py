6c0bfad1c1c6f10c81040600eef1a715f8d98596
Angel Hidalgo <ah.criptoy@gmail.com>
2017-01-31 16:04:42 -0500

    Corrección del programa 1, aquí se recorre el vector en orden inverso.

--- Vectores/Programa1.py
+++ Vectores/Programa1.py
@@ -1,9 +1,10 @@
 # -*- coding: cp1252 -*-
 # Programa que imprime los elementos de un vector, pero a partir de la última posición.
 vec = [5,4,6,8,9,7,10]
-i = 0
+i = len(vec) - 1
 
-while (i < 8):
-    i = i + 1
+while (i >= 0):
+    print vec[i]
+    i = i - 1
 
-print vec[::-1]
+
