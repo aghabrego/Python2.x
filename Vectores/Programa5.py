f0d5d1f10ff48413f70dc2afb2423c573d6414ee
Angel Hidalgo <ah.criptoy@gmail.com>
2017-01-31 20:04:57 -0500

    Acomodando espacio entre las asignaciones.

--- Vectores/Programa5.py
+++ Vectores/Programa5.py
@@ -1,9 +1,9 @@
 # Programa que coloca los elementos de un vector en otro.
-n=input("Cantidad de Veces ")
-vector=[]
+n = input("Cantidad de Veces ")
+vector = []
 
 for i in range (n):
-    elemento=input("Indique un valor ")
+    elemento = input("Indique un valor ")
     vector.append(elemento)
     
 vector2 = vector[:]
