9d79ac7c05310f073474bca366a8ad033872b633
Angel Hidalgo <ah.criptoy@gmail.com>
2017-01-31 20:03:18 -0500

    Agregando programa que coloca los elementos de un vector en otro.

---
+++ Vectores/Programa5.py
@@ -0,0 +1,10 @@
+# Programa que coloca los elementos de un vector en otro.
+n=input("Cantidad de Veces ")
+vector=[]
+
+for i in range (n):
+    elemento=input("Indique un valor ")
+    vector.append(elemento)
+    
+vector2 = vector[:]
+print "Nueva lista ",vector2
