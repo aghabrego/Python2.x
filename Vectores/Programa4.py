f65d0cf6e3e0576ec6b960f8aed108be20c32635
Angel Hidalgo <ah.criptoy@gmail.com>
2017-01-31 19:38:57 -0500

    Iniciando variable para obtener el mayo con el primer valor del vector.

--- Vectores/Programa4.py
+++ Vectores/Programa4.py
@@ -1,12 +1,13 @@
 # Programa que determina el mayor elemento de un vector.
 n = input("Cantidad de Veces ")
 vector = []
-mayor = 0
 
 for i in range(n):
     elemento = input("Indique un numero ")
     vector.append(elemento)
-    
+
+mayor = vector[0]
+
 for i in range(n):
     if vector[i] > mayor:
         mayor = vector[i]
